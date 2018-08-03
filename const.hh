afdceec5cc97d395b35e8ac3b46a0282cc546ea5
Ahmet Inan <inan@aicodix.de>
2018-08-03 21:14:24 +0200

    added half and fourth Pi constant

--- const.hh
+++ const.hh
@@ -11,6 +11,8 @@ namespace DSP {
 
 template <typename T>
 struct Const {
+	static inline T FourthPi()	{ return   0.78539816339744830962; } // 1*a(1)
+	static inline T HalfPi()	{ return   1.57079632679489661923; } // 2*a(1)
 	static inline T Pi()		{ return   3.14159265358979323846; } // 4*a(1)
 	static inline T TwoPi()		{ return   6.28318530717958647693; } // 8*a(1)
 	static inline T FourPi()	{ return  12.56637061435917295385; } // 16*a(1)
