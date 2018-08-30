8951bd140439c2230e0e4eabc2161cb8c71bd208
Ahmet Inan <inan@aicodix.de>
2018-08-30 08:40:20 +0200

    constexpr implies inline on functions

--- const.hh
+++ const.hh
@@ -11,12 +11,12 @@ namespace DSP {
 
 template <typename T>
 struct Const {
-	static inline constexpr T EighthPi()	{ return   0.39269908169872415481; } // a(1)/2
-	static inline constexpr T FourthPi()	{ return   0.78539816339744830962; } // 1*a(1)
-	static inline constexpr T HalfPi()	{ return   1.57079632679489661923; } // 2*a(1)
-	static inline constexpr T Pi()		{ return   3.14159265358979323846; } // 4*a(1)
-	static inline constexpr T TwoPi()	{ return   6.28318530717958647693; } // 8*a(1)
-	static inline constexpr T FourPi()	{ return  12.56637061435917295385; } // 16*a(1)
+	static constexpr T EighthPi()	{ return   0.39269908169872415481; } // a(1)/2
+	static constexpr T FourthPi()	{ return   0.78539816339744830962; } // 1*a(1)
+	static constexpr T HalfPi()	{ return   1.57079632679489661923; } // 2*a(1)
+	static constexpr T Pi()		{ return   3.14159265358979323846; } // 4*a(1)
+	static constexpr T TwoPi()	{ return   6.28318530717958647693; } // 8*a(1)
+	static constexpr T FourPi()	{ return  12.56637061435917295385; } // 16*a(1)
 };
 
 }
