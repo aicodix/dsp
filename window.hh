aicodix/dsp

commit 98a5a204d1b47e28433aed767bf2e5c88d784985

removed std:: from math functions

window.hh
@@ -75,7 +75,7 @@ public:
 	Gauss(TYPE o) : o(o) {}
 	TYPE operator () (int n, int N)
 	{
-		return std::exp(- TYPE(0.5) * std::pow((TYPE(n) - TYPE(N - 1) / TYPE(2)) / (o * TYPE(N - 1) / TYPE(2)), TYPE(2)));
+		return exp(- TYPE(0.5) * pow((TYPE(n) - TYPE(N - 0) / TYPE(2)) / (o * TYPE(N - 1) / TYPE(2)), TYPE(2)));
 	}
 };
 
@@ -109,7 +109,7 @@ public:
 	Kaiser(TYPE a) : a(a) {}
 	TYPE operator () (int n, int N)
 	{
-		return i0(Const<TYPE>::Pi() * a * std::sqrt(TYPE(1) - std::pow(TYPE(2 * n) / TYPE(N - 1) - TYPE(1), TYPE(2)))) / i0(Const<TYPE>::Pi() * a);
+		return i0(Const<TYPE>::Pi() * a * sqrt(TYPE(1) - pow(TYPE(2 * n) / TYPE(N - 1) - TYPE(1), TYPE(2)))) / i0(Const<TYPE>::Pi() * a);
 	}
 };
 

coeffs.hh
@@ -36,7 +36,7 @@ public:
 		for (int n = 0; n < TAPS; ++n)
 			sum += w[n];
 		for (int n = 0; n < TAPS; ++n)
-			w[n] /= divisor * std::abs(sum);
+			w[n] /= divisor * abs(sum);
 	}
 	inline TYPE operator () (int n) { return n >= 0 && n < TAPS ? w[n] : 0; }
 	inline operator const TYPE * () const { return w; }

utils.hh
@@ -25,13 +25,13 @@ AB lerp(X x, AB a, AB b)
 template <typename TYPE>
 TYPE normal_pdf(TYPE x, TYPE m, TYPE s)
 {
-	return std::exp(-std::pow((x - m) / s, TYPE(2)) / TYPE(2)) / (Const<TYPE>::SqrtTwoPi() * s);
+	return exp(-pow((x - m) / s, TYPE(2)) / TYPE(2)) / (Const<TYPE>::SqrtTwoPi() * s);
 }
 
 template <typename TYPE>
 TYPE sinc(TYPE x)
 {
-	return TYPE(0) == x ? TYPE(1) : std::sin(Const<TYPE>::Pi() * x) / (Const<TYPE>::Pi() * x);
+	return TYPE(0) == x ? TYPE(1) : sin(Const<TYPE>::Pi() * x) / (Const<TYPE>::Pi() * x);
 }
 
 template <typename TYPE>
@@ -43,7 +43,7 @@ TYPE delta(TYPE x)
 template <typename TYPE>
 TYPE decibel(TYPE v)
 {
-	return TYPE(10) * std::log10(v);
+	return TYPE(10) * log10(v);
 }
 
 }

commit 0427f29ce5c071da010f6f5935c3708078426513

fixed bug in Gaussian window introduced 2019
Typo in commit: 98a5a204d1b47e28433aed767bf2e5c88d784985

window.hh
@@ -74,7 +74,7 @@ public:
 	Gauss(TYPE o) : o(o) {}
 	TYPE operator () (int n, int N)
 	{
-		return exp(- TYPE(0.5) * pow((TYPE(n) - TYPE(N - 0) / TYPE(2)) / (o * TYPE(N - 1) / TYPE(2)), TYPE(2)));
+		return exp(- TYPE(0.5) * pow((TYPE(n) - TYPE(N - 1) / TYPE(2)) / (o * TYPE(N - 1) / TYPE(2)), TYPE(2)));
 	}
 };
 