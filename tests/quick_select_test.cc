c0347030c7214adc6f657284201fbdee4c86c810
Ahmet Inan <inan@aicodix.de>
2024-03-10 22:53:04 +0100

    check entire array

--- tests/quick_select_test.cc
+++ tests/quick_select_test.cc
@@ -7,7 +7,6 @@ Copyright 2024 Ahmet Inan <inan@aicodix.de>
 #include <random>
 #include <cassert>
 #include <iostream>
-#include <algorithm>
 #include <functional>
 #include "quick_select.hh"
 
@@ -22,15 +21,18 @@ int main()
 	typedef std::default_random_engine generator;
 	typedef std::uniform_int_distribution<int> distribution;
 	auto rand = std::bind(distribution(1, MAX_N), generator(seed));
-	int a[MAX_N], b[MAX_N];
+	int a[MAX_N];
 	for (int loop = 0; loop < 10000; ++loop) {
 		int size = rand();
 		for (int i = 0; i < size; ++i)
-			a[i] = b[i] = rand();
+			a[i] = rand();
 		int k = rand() % size;
-		std::nth_element(a, a+k, a+size);
-		int v = DSP::quick_select(b, k, size);
+		int v = DSP::quick_select(a, k, size);
 		assert(v == a[k]);
+		for (int i = 0; i < k; ++i)
+			assert(a[i] <= a[k]);
+		for (int i = k + 1; i < size; ++i)
+			assert(a[i] >= a[k]);
 	}
 	std::cerr << "Quick select algorithm test passed!" << std::endl;
 	return 0;
